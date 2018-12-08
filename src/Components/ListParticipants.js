ae8486e2bdfac69aa38b59e4616b762a35e5ea01
alvaroha1 <alvarohernandezassens@gmail.com>
2018-12-09 00:36:07 +0100

    feat: participants not anymore hardcoded

--- src/Components/ListParticipants.js
+++ src/Components/ListParticipants.js
@@ -2,26 +2,17 @@ import React from 'react';
 import Participant from './Participant';
 import NewParticipant from './NewParticipant';
 
-const listParticipants = (props) => {
-  const currentParticipants = props.participants.map((name, i)=>(
-		<Participant
-			key={i}
-			
-		/>
-	))
-   
-  
-  return (
-    <div className="card border-dark mb-3">
-        <h4>Participants</h4>
-          {currentParticipants}
-          <Participant name="Max Mustermann"></Participant>
-          <Participant name="Kevin Müller"></Participant>
-          <Participant name="Dennis Mustermann"></Participant>
-          <Participant name="Donald Trump"></Participant>
-          <NewParticipant></NewParticipant>
-        </div>
-  );
-} 
-
-export default listParticipants;
+class ListParticipants extends React.Component {
+  render() {
+    return (
+      <div className="card border-dark mb-3">
+          <h4>Participants</h4>
+            {this.props.participants.map (participant => {
+              return <Participant name={participant.name}/>
+            })}
+            <NewParticipant></NewParticipant>
+      </div>
+    );
+  } 
+}
+export default ListParticipants;
